bd04919002be2631d47288d827fd317f8f9f935e
Nisschhal <mrnischalpuri@gmail.com>
2023-02-01 11:39:28 +0545

    db connected and added colors to message

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,15 @@
 const mongoose = require("mongoose");
-
-const connectDB = async () => {};
+require("colors");
+require("dotenv").config();
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `MongoDb Connected: ${conn.connection.host}`.blue.bold.underline
+    );
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,28 @@ const express = require("express");
 const products = require("./data/products");
 const cors = require("cors");
 const app = express();
+const connectDB = require("./config/db");
+require("dotenv").config();
+require("colors");
+
+// DB CONNECTION
+connectDB();
+
+// CORS USE
 app.use(cors());
+
+// GET PRODUCTS
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
+// GET PRODUCT BY ID
 app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id == req.params.id);
   res.json(product);
 });
 
-app.listen(5000, () => {
-  console.log("Server Running on 5000!");
+// CONNECT SERVER TO PORT 5000
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server Running on ${process.env.PORT}`.yellow.bold);
 });
